591b97e8a16ed4530f7059ee2effb1d656b79db7
Chace-Berry <chaceberry686@gmail.com>
2026-04-26 15:49:09 +0200

    chore: bump to 1.1.5, upgrade TS to 6.x, fix README license and badge

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,4 +1,4 @@
-import { pack as encode, unpack as decode } from "msgpackr";
+import { encode, decode } from "@msgpack/msgpack";
 import { x25519 } from "@noble/curves/ed25519.js";
 import { hmac } from "@noble/hashes/hmac.js";
 import { sha256 } from "@noble/hashes/sha2.js";
